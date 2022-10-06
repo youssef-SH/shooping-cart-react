c314ec3d00e155b39a0be738b05391998c5f0057
youssef-SH <youssef.shaban.145@gmail.com>
2022-10-06 22:11:19 +0200

    footer comp

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../css/Footer/Footer.css"
+
+function Footer() {
+    return (
+        <footer>
+            &copy; copyWright Youssef shaaban 2022
+        </footer>
+    )
+}
+
+export default Footer
