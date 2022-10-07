13f8b7586fd8e90364af4f44b7f3c972d0017a09
youssef-SH <youssef.shaban.145@gmail.com>
2022-10-07 17:31:16 +0200

    products comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
+import React , { useState } from 'react';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import { words } from "./words";
+import Products from './components/Products/Products';
+// import { words } from "./words";
+import data from './data.json'
+
+
 function App () {
+
+  const [products, setProducts] = useState(data)
+
   return (
     <div className="layout">
       <Header />
 
       <main>
-        {words.content}
+        <div className="wrapper">
+          <Products products={products} />
+          <div className="filter-wrapper"> Filter </div>
+        </div>
       </main>
 
       <Footer />
---
+++ client/src/components/Products/Products.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "../../css/Products/Products.css"
+
+function Products(props) {
+  return (
+    <div className="products-wrapper"> {props.products.map(product => (
+        <div className="product-item" key={product.id}>
+            <img src={product.imageUrl} alt={product.title} />
+            <div className="product-desc">
+                <p> {product.title}</p>
+                <span>{product.price}</span>
+
+            </div>
+            <button> Add to Cart </button>
+
+          </div>
+          ))}
+
+     </div>
+  )
+}
+export default Products
