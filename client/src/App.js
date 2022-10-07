1511bfa2da10a4f93c7a91aa8d0b2b1baa062d65
youssef-SH <youssef.shaban.145@gmail.com>
2022-10-07 22:55:59 +0200

    filter-comp

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,40 @@ import Filter from './components/Filter/Filter';
 function App () {
 
   const [products, setProducts] = useState(data)
+  const [sort, setSort] = useState("");
+  const [company, setCompany] = useState("");
+
+  const handleFilterByCompany = (e) => {
+      setCompany(e.target.value);
+      if(e.target.value == "All"){
+        setProducts(data);
+      } else {
+        let productsClone = [...products];
+        let newProducts = productsClone.filter(p => p.company.indexOf(e.target.value) != -1);
+        setProducts(newProducts);
+
+      }
+  }
+
+  const handleFilterByOrder = (e) => {
+      let order = e.target.value;
+      setSort(order);
+      let productsClone = [...products];
+      let newProducts = productsClone.sort(function(a,b) {
+        if(order == "lowest") {
+          return a.price - b.price
+        }
+        else if(order == "highest") {
+          return b.price - a.price
+        }
+        else {
+          return a.id < b.id ? 1 : -1
+        }
+      });
+
+      setProducts(newProducts)
+
+  }
 
   return (
     <div className="layout">
@@ -18,7 +52,12 @@ function App () {
       <main>
         <div className="wrapper">
           <Products products={products} />
-          <Filter />
+          <Filter
+            company={company}
+            sort={sort}
+            handleFilterByOrder={handleFilterByOrder}
+            handleFilterByCompany={handleFilterByCompany}  
+            />
         </div>
       </main>
 
--- client/src/components/Filter/Filter.js
+++ client/src/components/Filter/Filter.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import "../../css/Filter/Filter.css"
 
-function Filter() {
+function Filter(props) {
   return (
     <div className="filter-wrapper">
         <h2 className='filter-title'>Filter</h2>
         <div className='num-of-products'> Number Of Products : 3</div>
         <div className='filter-by-company'>
             <span> Filter </span>
-            <select className='filter-select'>
+            <select value={props.company} className='filter-select' onChange={props.handleFilterByCompany}>
                 <option value="All">All</option>
                 <option value="iphone">iphone</option>
                 <option value="samsung">samsung</option>
@@ -18,9 +18,9 @@ function Filter() {
 
         <div className='filter-by-order'>
             <span> Order </span>
-            <select className='filter-select'>
+            <select className='filter-select' value={props.sort} onChange={props.handleFilterByOrder}>
                 <option value="latest">latest</option>
-                <option value="lower">lower</option>
+                <option value="lowest">lowest</option>
                 <option value="highest">highest</option>
             </select>
         </div>
